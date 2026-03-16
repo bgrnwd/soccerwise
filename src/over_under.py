7de74ee7a4765955fd8094a10c1e6e857fcd7e85
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2026-03-16 08:17:45 +0000

    data: update

--- src/over_under.py
+++ src/over_under.py
@@ -40,7 +40,7 @@ st.html(
 )
 
 st.caption(
-    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Sunday March 15, 2026 at 08:16:22 AM UTC."
+    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Monday March 16, 2026 at 08:17:45 AM UTC."
 )
 st.caption(
     "The wordmarks, logos, trade names, packaging and designs of MLS, SUM, the current and former MLS member clubs are the exclusive property of MLS or their affiliates."
