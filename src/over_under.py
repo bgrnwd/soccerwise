442ed3b2e2eb22ac414030bd368c76bf20845480
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2026-09-20 12:56:32 +0000

    data: update

--- src/over_under.py
+++ src/over_under.py
@@ -21,7 +21,7 @@ LABELS = {
     "points": "Points",
 }
 CAPTIONS = [
-    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Thursday September 17, 2026 at 12:54:51 PM UTC.",
+    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Sunday September 20, 2026 at 12:56:32 PM UTC.",
     "The wordmarks, logos, trade names, packaging and designs of MLS, SUM, the current and former MLS member clubs are the exclusive property of MLS or their affiliates.",
     "Data courtesy of [American Soccer Analysis](https://www.americansocceranalysis.com/).",
 ]
